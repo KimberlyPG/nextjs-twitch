6f05a9f9f15e0b0be235e3fb4742ae76d8cc2f9c
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-08-18 16:30:28 -0600

    live tag in games page

--- pages/game.jsx
+++ pages/game.jsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
 import Layout from '../components/Layout';
+import { useAppSelector } from "../store/hooks";
+
+import { selectRecommended } from "../store/slices/recommended/recommendedSlice";
 
 const Game = () => {
     const { data: session, status } = useSession();
@@ -12,8 +15,7 @@ const Game = () => {
     const gameId = router.query.id;
     const gameName = router.query.name;
     const gameUrl = router.query.image;
-    console.log("game", gameId, gameName, gameUrl);
-
+    
     const [channel, setChannel] = useState([]);
  
     useEffect(() => {
@@ -44,17 +46,21 @@ const Game = () => {
                             className="h-56 bg-purple-500 shadow-lg shadow-purple-500/50" 
                             src={gameUrl.slice(0, -21)+".jpg"} alt="" 
                         />
-                        <h4 className="text-4xl pl-5">{gameName}</h4>                        
+                        <h2 className="text-4xl pl-5">{gameName}</h2>                        
                     </span>
                 </div>
 
-                <div className="grid grid-cols-4 grid-flow-row place-items-center pt-3">
+                <div className="grid grid-cols-5 grid-flow-row place-items-center pt-3">
                     {channel && channel.map((streamer) => (
                         <div className="pb-7 cursor-pointer hover:opacity-80">
-                            <img
-                                className="w-80" 
-                                src={streamer.thumbnail_url.slice(0, -21)+".jpg"} alt="" />
-                            <h4 className="text-xs w-80 truncate">{streamer.title}</h4>
+                            <div className="relative">
+                                <h4 className="m-1 bg-red-500 w-10 h-4 text-xs rounded-md text-center absolute">LIVE</h4>
+                                <img
+                                    className="w-80" 
+                                    src={streamer.thumbnail_url.slice(0, -21)+".jpg"} alt="" 
+                                />
+                            </div>
+                            <h3 className="text-xs w-80 truncate">{streamer.title}</h3>
                             <h4 className="text-xs text-gray-400">{streamer.user_name}</h4>
                         </div>
                     ))}
