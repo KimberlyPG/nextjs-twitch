ba46e346916ce9f6e56ee0b058e9a0e577165a48
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-08-24 17:25:25 -0600

    cleaning imports

--- pages/game.jsx
+++ pages/game.jsx
@@ -1,7 +1,5 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 
 import Layout from '../components/Layout';
