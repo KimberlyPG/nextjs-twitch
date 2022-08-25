40817603e4dcbf2e63ac362bc6075f40283efd81
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-08-25 16:19:33 -0600

    react-embed-video imported dynamic

--- pages/stream.jsx
+++ pages/stream.jsx
@@ -1,11 +1,15 @@
-import ReactTwitchEmbedVideo from "react-twitch-embed-video";
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
+
+const ReactTwitchEmbedVideo = dynamic(() => import('react-twitch-embed-video'), {
+  ssr: false,
+})
 
 const Stream = () => {
   const router = useRouter();
   const streamer = router.query.streamer;
-  
+
   console.log(router.query);
     return (
           <Layout>
