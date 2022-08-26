8290e11724b7fced2f280d3bccc8d70afcd06c08
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-08-25 18:33:47 -0600

    space between imports

--- pages/stream.jsx
+++ pages/stream.jsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
 
 const ReactTwitchEmbedVideo = dynamic(() => import('react-twitch-embed-video'), {
