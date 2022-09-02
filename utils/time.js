da12439a8087f1de437dcc5ef073c6700e2d5052
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-09-01 18:42:29 -0600

    using the video hours length as a param for slice method

--- utils/time.js
+++ utils/time.js
@@ -22,47 +22,16 @@ export function time(duration) {
         const minutes = format.split(":")[1];
         const seconds = format.split(":")[2];
 
-        if(hours.length == 3) {
-            if(minutes < 10 && seconds > 9 ) {
-                return duration = format.slice(0, 4) + "0" + format.slice(4);
-            }
-            if(minutes < 10 && seconds < 10) {
-                return duration = format.slice(0, 4) + "0" + format.slice(4, 6) + "0" + format.slice(6);
-            }
-            if(minutes > 9 && seconds < 10 )
-                return duration = format.slice(0, 7) + "0" + format.slice(7);
-            else {
-                return format;
-            }
+        if(minutes < 10 && seconds > 9 ) {
+            return duration = format.slice(0, hours.length+1) + "0" + format.slice(hours.length+1);
         }
-
-        if(hours.length == 2) {
-            if(minutes < 10 && seconds > 9 ) {
-                return duration = format.slice(0, 3) + "0" + format.slice(3);
-            }
-            if(minutes < 10 && seconds < 10) {
-                return duration = format.slice(0, 3) + "0" + format.slice(3, 5) + "0" + format.slice(5);
-            }
-            if(minutes > 9 && seconds < 10 )
-                return duration = format.slice(0, 6) + "0" + format.slice(6);
-            else {
-                return format;
-            }
+        if(minutes < 10 && seconds < 10) {
+            return duration = format.slice(0, hours.length+1) + "0" + format.slice(hours.length+1, 6) + "0" + format.slice(hours.length+3);
         }
-   
-        if(hours.length == 1) {
-            if(minutes < 10 && seconds > 9 ) {
-                return duration = format.slice(0, 2) + "0" + format.slice(2);
-            }
-            if(minutes < 10 && seconds < 10) {
-                return duration = format.slice(0, 2) + "0" + format.slice(2, 4) + "0" + format.slice(4);
-            }
-            if(minutes > 9 && seconds < 10 )
-                return duration = format.slice(0, 5) + "0" + format.slice(5);
-            else {
-                return format;
-            }
+        if(minutes > 9 && seconds < 10 )
+            return duration = format.slice(0, hours.length+4) + "0" + format.slice(hours.length+4);
+        else {
+            return format;
         }
-
     }
 }
