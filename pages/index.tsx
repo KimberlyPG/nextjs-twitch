7e332c5d37b663407dc644aa15b48983f820b8b6
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-10 14:34:37 -0600

    improving the indentation and dependencies added to the useeffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,34 @@ import Twitch from "../components/Twitch";
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  const currentUser = session?.user?.name;
-  const router = useRouter();
+	const { data: session } = useSession();
+	const currentUser = session?.user?.name;
+	const router = useRouter();
 
-  useEffect(() => {
-  if (!session) {
-    console.log("Not Authenticated" );
-      router.push('/login')
-    }
-  }, [currentUser]);
-  console.log("current user:", currentUser);
+	useEffect(() => {
+		if (!session) {
+			console.log("Not Authenticated" );
+			router.push('/login')
+		}
+	}, [currentUser, router, session]);
 
-  return (
-    <div className='bg-black h-100 w-100 flex'>
-      <Layout>
-        <Twitch />
-      </Layout>
-    </div>
-  );
-}
+	return (
+		<div className='bg-black h-100 w-100 flex'>
+			<Layout>
+				<Twitch />
+			</Layout>
+		</div>
+	);
+	}
 
-export async function getServerSideProps(context: {}) {
-  const session = await getSession(context);
+	export async function getServerSideProps(context: {}) {
+	const session = await getSession(context);
 
-  return {
-    props: {
-      session
-    },
-  };
-}
+		return {
+				props: {
+					session
+				},
+			};
+		}
 
-export default Home;
+	export default Home;
