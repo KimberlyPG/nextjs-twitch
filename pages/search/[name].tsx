533f7308d3420e75c70a796aeb40ff4e48387250
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-19 22:14:57 -0600

    migrating page to typescript

--- pages/search/[name].tsx
+++ pages/search/[name].tsx
@@ -1,17 +1,18 @@
+import { NextPage } from "next";
 import SearchList from "../../components/SearchList";
 import Layout from '../../components/Layout';
 
 import { useAppSelector } from "../../store/hooks";
-
 import { selectSearch } from "../../store/slices/searchSlice/searchSlice";
+import { SearchChannels } from "../../types/types";
 
-const Search = () => {
+const Search: NextPage = () => {
     const results = useAppSelector(selectSearch);
 
     return (
         <Layout>
             <div className="pt-10 font-roboto">
-                {results?.map((streams) => (
+                {results?.map((streams: SearchChannels) => (
                     <SearchList key={streams.id} streams={streams}/>
                 ))}
             </div>
