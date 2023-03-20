393869c73069592b391c184381199771ab59af63
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-19 21:48:18 -0600

    types for the component props

--- types/types.ts
+++ types/types.ts
@@ -60,3 +60,23 @@ export interface Follow {
     to_name:     string;
     followed_at: string;
 }
+
+export interface Video {
+    id:             string;
+    stream_id:      string;
+    user_id:        string;
+    user_login:     string;
+    user_name:      string;
+    title:          string;
+    description:    string;
+    created_at:     string;
+    published_at:   string;
+    url:            string;
+    thumbnail_url:  string;
+    viewable:       string;
+    view_count:     number;
+    language:       string;
+    type:           string;
+    duration:       string;
+    muted_segments: null;
+}
