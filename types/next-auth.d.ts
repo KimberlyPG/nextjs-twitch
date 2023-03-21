b773eb39c0aab0c6b5d3ff5ab3e5f2cb2eb0e5c1
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-21 12:17:19 -0600

    module argumentation and secret value for nextauth

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,5 @@
-import NextAuth, { DefaultSession } from "next-auth"
+import NextAuth, { Account, DefaultSession, User } from "next-auth"
+import { DefaultJWT, JWT } from "next-auth/jwt"
 
 declare module "next-auth" {
     interface Session {
@@ -10,25 +11,48 @@ declare module "next-auth" {
         name:  string;
         email: string;
         image: string;
-        token: string;
-        id:    string;
-        }
-}
+        id?:    string;
+    }
 
-import { JWT } from "next-auth/jwt"
-
-declare module "next-auth/jwt" {
-    interface JWT {
+    interface Token {
         name: string;
         email: string;
         picture: string;
         sub: number;
         accessToken: string;
         userName: string;
-        accessTokenExpires: number;
+        accessTokenExpires?: number;
         id: number;
         iat: number;
         exp: number;
         jti: string;
+    }
+    
+    interface Account {
+        provider: string;
+        type: string;
+        providerAccountId: string;
+        access_token: string;
+        expires_at: number;
+        id_token: string;
+        refresh_token: string;
+        scope: string;
+        token_type: string;
+    }
+
+    interface Profile {
+        id: number;
+        name: string;
+        email: string;
+        image: string;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        accessToken: string;
+        username: string;
+        accessTokenExpires: number;
+        id: number;
     }
 }
