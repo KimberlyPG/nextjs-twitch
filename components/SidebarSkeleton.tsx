3f27290eb4ad33d4b4dcdc76375428e33d406e13
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-28 17:30:01 -0600

    new styles for the skeleton

--- components/SidebarSkeleton.tsx
+++ components/SidebarSkeleton.tsx
@@ -10,27 +10,35 @@ import 'react-loading-skeleton/dist/skeleton.css'
 const SidebarSkeleton = () => {
 	return (
 		<div className="pt-10 h-screen space-y-5">
-			<Skeleton  height={16} baseColor="#202020" highlightColor="#444" />
-			<div className="flex flex-col items-center h-2/5 w-80 overflow-y-scroll scrollbar-hide lg:max-w-[14rem] xs:max-w-[4rem] space-y-5">
+			<div className="flex xs:hidden lg:flex pl-5">
+				<Skeleton width={60} height={10} baseColor="#202020" highlightColor="#444" />
+			</div>
+			<div className="flex flex-col items-center h-2/5 w-80 overflow-y-scroll scrollbar-hide lg:max-w-[14rem] xs:max-w-[4rem]">
 				{Array.from({ length: 10 }, (_, i) => (
-					<div key={i} className="flex flex-row py-2 pl-4 pr-2">
+					<div key={i} className="flex flex-row py-2 pr-2 space-x-1">
 						<Skeleton circle height={32} width={32} baseColor="#202020" highlightColor="#444" />
-						<div className="flex flex-col xs:hidden lg:flex space-y-1">
+						<div className="flex flex-col xs:hidden lg:flex">
 							<Skeleton count={2} width={112} height={13} baseColor="#202020" highlightColor="#444" />
 						</div>
-						<Skeleton  width={28} height={13} baseColor="#202020" highlightColor="#444" />
+						<div className="flex xs:hidden lg:flex">
+							<Skeleton  width={28} height={13} baseColor="#202020" highlightColor="#444" />
+						</div>
 					</div>
 				))}
 			</div>
-			<Skeleton  height={16} baseColor="#202020" highlightColor="#444" />
-			<div className="flex flex-col items-center h-2/5 w-80 overflow-y-scroll scrollbar-hide lg:max-w-[14rem] xs:max-w-[4rem] space-y-5">
+			<div className="flex xs:hidden lg:flex pl-5">
+				<Skeleton width={60} height={10} baseColor="#202020" highlightColor="#444" />
+			</div>
+			<div className="flex flex-col items-center h-2/5 w-80 overflow-y-scroll scrollbar-hide lg:max-w-[14rem] xs:max-w-[4rem]">
 				{Array.from({ length: 10 }, (_, i) => (
-					<div key={i} className="flex flex-row py-2 pl-4 pr-2 w-full">
+					<div key={i} className="flex flex-row py-2 pr-2 space-x-1">
 						<Skeleton circle height={32} width={32} baseColor="#202020" highlightColor="#444" />
-							<div className="flex flex-col xs:hidden lg:flex space-y-1">
+							<div className="flex flex-col xs:hidden lg:flex">
 								<Skeleton count={2} width={112} height={13} baseColor="#202020" highlightColor="#444"  />
 							</div>
-						<Skeleton  width={28} height={13} baseColor="#202020" highlightColor="#444" />
+							<div className="flex xs:hidden lg:flex">
+								<Skeleton  width={28} height={13} baseColor="#202020" highlightColor="#444" />
+							</div>
 					</div>
 				))}
 			</div>
