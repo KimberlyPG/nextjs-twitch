3ca10457a63ab594d039a982e8d8c98db8a7cb7e
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-30 13:08:48 -0600

    configuring swr globally in the layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,12 +3,17 @@ import { FC, ReactNode } from "react";
 import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 
+import { SWRConfig } from 'swr';
+import useFetcher from '../hooks/useFetcher';
+
 type LayoutProps = {
     children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+    const fetcher = useFetcher();
     return (
+        <SWRConfig value={{  refreshInterval: 120000, fetcher }} >
         <div className='flex h-screen w-screen overflow-y-scroll scrollbar-hide bg-black'>
             <Sidebar />
             <div className='flex flex-col h-full w-full'>
@@ -16,6 +21,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
                 <div className='h-full overflow-y-scroll scrollbar-hide'>{children}</div>
             </div>
         </div>
+        </SWRConfig>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,27 @@
 import type { NextPage } from 'next'
-import { getSession, useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
-import Twitch from "../components/Twitch";
 import Layout from '../components/Layout';
-import { SWRConfig } from 'swr';
-import useFetcher from '../hooks/useFetcher';
+import Twitch from "../components/Twitch";
 
 const Home: NextPage = () => {
-	const fetcher = useFetcher();
-
 	return (
 		<div className='bg-black h-100 w-100 flex'>
-			<SWRConfig value={{  refreshInterval: 120000, fetcher }} >
-				<Layout>
-					<Twitch />
-				</Layout>
-			</SWRConfig>
+			<Layout>
+				<Twitch />
+			</Layout>
 		</div>
 	);
 	}
 
-	export async function getServerSideProps(context: {}) {
-	const session = await getSession(context);
+export async function getServerSideProps(context: {}) {
+const session = await getSession(context);
 
-		return {
-				props: {
-					session
-				},
-			};
-		}
+	return {
+			props: {
+				session
+			},
+		};
+	}
 
-	export default Home;
+export default Home;
