44e97e49df98d791b40d730d9b062a267db71820
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-30 17:50:53 -0600

    fetching the game streams with swr

--- hooks/useFetcher.ts
+++ hooks/useFetcher.ts
@@ -4,20 +4,19 @@ import twitch from "../pages/api/twitch";
 
 const useFetcher = () => {
     const { data: session, status } = useSession();
-	const currentToken = session?.user.token;
+	const currentToken = session?.user?.token;
 
-    const fetcher = async(url: string) => {
+	const fetcher = async(url: string) => {
 		return await twitch.get(url, 
 			{
 				headers: {
-				"Authorization": `Bearer ${currentToken}`,
-				"Client-Id": process.env.NEXT_PUBLIC_CLIENT_ID as string,
+					"Authorization": `Bearer ${currentToken}`,
+					"Client-Id": process.env.NEXT_PUBLIC_CLIENT_ID as string,
 				},
 			})
 			.then((res) => res.data.data)
 	}
-    
-    return fetcher;
+	return fetcher;
 }
 
 export default useFetcher;
--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -1,8 +1,8 @@
-import { useSession, getSession } from "next-auth/react";
-import { useState, useEffect } from "react";
+import { getSession, useSession } from "next-auth/react";
 import { NextPage, GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import useSWR from 'swr';
 
 import Layout from '../../components/Layout';
 import GameCards from "../../components/GameCard";
@@ -10,51 +10,39 @@ import GameCards from "../../components/GameCard";
 import twitch from "../api/twitch";
 import { GameData, LiveStreamsData } from "../../types/types";
 import { shimmer, toBase64 } from "../../utils/shimmerImage";
+import useFetcher from "../../hooks/useFetcher";
 
 type GameProps = {
     game: GameData;
+    gameId: string;
 }
 
 const Game: NextPage<GameProps> = ({ game }) => {
+    const fetcher = useFetcher();
     const { data: session, status } = useSession();
     const currentToken = session?.user.token;
 
     const router = useRouter();
     const gameId = router.query.id;
+ 
+    const { data: channel, error: followsError } = useSWR<LiveStreamsData[], Error>(`/streams?game_id=${gameId}&first=15`, fetcher, {refreshInterval: 120000});
 
-    const [channel, setChannel] = useState<LiveStreamsData[]>([]);
-
-    useEffect(() => {
-        if(gameId && currentToken) {
-        const getChannels = async () => {
-                await twitch.get(`/streams?game_id=${gameId}&first=15`,
-                    {
-                        headers: {
-                            "Authorization": `Bearer ${currentToken}`,
-                            "Client-Id": process.env.NEXT_PUBLIC_CLIENT_ID as string,
-                        }
-                    })
-                    .then(res => setChannel(res.data.data));
-                }
-            getChannels();
-            }
-    }, [currentToken, gameId]);
-
+    if(!channel) return <div>Loading...</div>
     return (
         <Layout>
             <div className="text-white font-roboto sm:p-5">
                 <div className="flex relative items-center my-10 m-10">
                     <Image
                         className="bg-purple-500 shadow-lg shadow-purple-500/50 object-contain lg:h-56 md:h-44 xs:h-36 w-fit" 
-                        src={game?.box_art_url?.split("-{width}x{height}").join('')} 
-                        alt={`${game?.name} image`} 
+                        src={game.box_art_url.split("-{width}x{height}").join('')} 
+                        alt={`${game.name} image`} 
                         width={168}
                         height={224}
                         priority={true}
                         placeholder="blur"
                         blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
                     />
-                    <h1 className="lg:text-4xl md:text-2xl sm:text-xl ml-5 w-full">{game?.name}</h1>                        
+                    <h1 className="lg:text-4xl md:text-2xl sm:text-xl ml-5 w-full">{game.name}</h1>                        
                 </div>
 
                 <h2 className="text-sm my-5 font-semibold">Live channels we think you will like</h2>
