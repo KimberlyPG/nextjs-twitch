84df898119e2798bb01343fc2aa6f28bd2f2f503
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-31 14:18:50 -0600

    new function that refresh token when the token has expired

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,28 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import axios from "axios";
+
+async function refreshAccessToken(token) {
+    const baseUrl = "https://id.twitch.tv/oauth2/token?"
+    try {     
+        const res = await axios.post(`${baseUrl}&grant_type=refresh_token&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_CLIENT_SECRET}&refresh_token=${token.refreshToken}`)
+        const refreshedToken = await res.data;
+
+        return {
+            ...token,
+            accessToken: refreshedToken.access_token,
+            accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, 
+            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+        };
+  
+    } catch(error) {
+        console.error(error);
+    
+        return {
+            ...token,
+            error: 'RefreshAccessTokenError',
+        };
+    }
+}
 
 export default NextAuth({
   // provider configuration
@@ -46,13 +70,16 @@ export default NextAuth({
     callbacks: {
         async jwt({ token, account, user }) {
             // initial sign in
+            console.log("account", account)
             if (account && user) {
+                console.log("user", user)
                 return{
                     ...token,
                     accessToken: account.access_token,
                     username: token.name,
                     accessTokenExpires: account.expires_at * 1000,
-                    id: token.sub
+                    id: token.sub,
+                    refreshToken: account.refresh_token
                 };
             }
 
@@ -61,9 +88,13 @@ export default NextAuth({
                 console.log("EXISTING ACCESS TOKEN IS VALID");
                 return token;
             }
+
+            // New access token when the access token has expired
+            console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
+            return await refreshAccessToken(token, account);
         },
 
-        async session({ session, token }) {      
+        async session({ session, token }) {   
             session.user.token = token.accessToken;
             session.user.name = token.username;
             session.user.id = token.sub;
