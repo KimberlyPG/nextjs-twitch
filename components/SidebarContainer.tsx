c3d6d785f9b988eb2eb4b2cc804cd4bf5b33150e
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-04-02 15:38:44 -0600

    max height changed for the sidebar containers

--- components/SidebarContainer.tsx
+++ components/SidebarContainer.tsx
@@ -20,7 +20,7 @@ const SidebarContainer: FC<SidebarContainerProps> = ({ title = "", children }) =
             {title === "Recommended Channels" &&
                 <BsCameraVideo className={`text-gray-500 w-full lg:hidden mt-4 ${toggleSidebar ? "xs:flex":"xs:hidden"}`} />
             }
-            <div className={`flex flex-col max-h-2/5 w-80 overflow-y-scroll scrollbar-hide 
+            <div className={`flex flex-col max-h-[40%] w-80 overflow-y-scroll scrollbar-hide 
             ${toggleSidebar ? "lg:max-w-[14rem] xs:max-w-[4rem]":"lg:hidden xs:hidden"}`}>
                 <div className="border-r border-gray-900">
                     {children}
