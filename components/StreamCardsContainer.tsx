9bdcfc4d9b7277a947e5f5d493f7a7c2582b7540
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-04-19 12:31:53 -0600

    change in the description text size

--- components/StreamCardsContainer.tsx
+++ components/StreamCardsContainer.tsx
@@ -13,7 +13,7 @@ type StreamCardContainerProps = {
 const StreamCardContainer: FC<StreamCardContainerProps> = ({ description, recommendedData, followedData }) => {
   return (
     <div className="xs:pt-2">
-        <h2 className="md:pb-5 xs:pb-3 xs:pl-2 font-semibold xs:text-xs md:text-lg">{description}</h2> 
+        <h2 className="md:pb-5 xs:pb-3 xs:pl-2 font-semibold xs:text-md md:text-lg">{description}</h2> 
         <div className="grid 3xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-1">
 		{description === "Recommended Channels" &&
 				recommendedData?.map((item) => {
