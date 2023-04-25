2afb143ef8ca51a0d281a0eeac5d469de799c49a
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-04-25 16:18:57 -0600

    live stream video added to the profile when the streamer is live

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -11,26 +11,45 @@ import { shimmer, toBase64 } from "../utils/shimmerImage";
 type VideoCardProps = {
     item: Video;
 }
+
 const VideoCard: FC<VideoCardProps> = ({ item }) => {
-    if(item.thumbnail_url === "https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png") return null;
     return (
         <div className='cursor-pointer hover:opacity-80 mb-5 mx-1'>
             <Link href={`/videos/${item.id}`}>
                 <div className="space-y-2">
                     <div className="relative">
-                        <Image 
-                            className="hover:opacity-80 object-contain w-full h-full" 
-                            src={item.thumbnail_url.slice(0, -22)+"572x318.jpg"} 
-                            alt="stream image"
-                            priority={true}
-                            width={570}
-                            height={318}
-                            placeholder="blur"
-                            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
-                        />
-                        <p className="m-2 text-sm text-white bg-black bg-opacity-60 absolute top-0">{time(item.duration)}</p>
-                        <p className="m-2 text-sm text-white bg-black bg-opacity-60 absolute bottom-0">{viewersFormat(item.view_count)} views</p>
-                        <p className="m-2 text-sm text-white bg-black bg-opacity-60 absolute bottom-0 right-0">{days(item.created_at, item.duration)}</p>
+                        {item.thumbnail_url === "https://vod-secure.twitch.tv/_404/404_processing_%{width}x%{height}.png" ? (
+                            <Image 
+                                className="hover:opacity-80 object-contain w-full h-full" 
+                                src="https://vod-secure.twitch.tv/_404/404_processing_320x180.png"
+                                alt={`${item.user_name} live stream image`}
+                                priority={true}
+                                width={570}
+                                height={318}
+                                placeholder="blur"
+                                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+                            />
+                        ):(
+                            <Image 
+                                className="hover:opacity-80 object-contain w-full h-full" 
+                                src={item.thumbnail_url.slice(0, -22)+"572x318.jpg"} 
+                                alt={`${item.user_name} stream image`}
+                                priority={true}
+                                width={570}
+                                height={318}
+                                placeholder="blur"
+                                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+                            />
+                        )}
+                        <p className="m-2 text-sm text-white bg-black bg-opacity-60 absolute top-0">
+                            {time(item.duration)}
+                        </p>
+                        <p className="m-2 text-sm text-white bg-black bg-opacity-60 absolute bottom-0">
+                            {viewersFormat(item.view_count)} views
+                        </p>
+                        <p className="m-2 text-sm text-white bg-black bg-opacity-60 absolute bottom-0 right-0">
+                            {days(item.created_at, item.duration)}
+                        </p>
                     </div>
                     <p className='w-full truncate sm:text-sm xs:text-xs'>{item.title}</p>
                 </div>
--- pages/profile/[name].tsx
+++ pages/profile/[name].tsx
@@ -26,7 +26,7 @@ const Profile: NextPage<ProfileProps> = ({ userData }) => {
     const state = router.query.state;
     const userId = router.query.name;
 
-    const { data: video, error, isLoading } = useSWR<Video[]>(`/videos?user_id=${userId}&first=6`);
+    const { data: video, error, isLoading } = useSWR<Video[]>(`/videos?user_id=${userId}&first=5`);
     const { data: follows } = useSWR<Video[]>(`/channels/followers?broadcaster_id=${userId}`);
     console.log(follows)
     if(isLoading) return <ProfileSkeleton />
@@ -39,43 +39,36 @@ const Profile: NextPage<ProfileProps> = ({ userData }) => {
                 height="450px"
                 layout='video'
             /> 
-            <div className='w-full'>
-                {state === "true" &&
-                    <div className='pl-3'>
-                        <Link href={`/stream/${userData?.display_name}`}>
-                            <p className='bg-red-600 text-center rounded-sm font-bold font-roboto p-1 hover:opacity-50 w-28 xs:text-sm lg:text-lg'>GO TO LIVE</p>
-                        </Link>
-                    </div>
-                }
-                <div className='flex flex-row items-center m-5'>
-                    <Image 
-                        className="rounded-full cursor-pointer" 
-                        src={userData?.profile_image_url}
-                        width={70}
-                        height={70}
-                        priority
-                        alt="user image" 
-                    /> 
-                    <h3 className='pl-5'>{userData?.display_name}</h3>
+            {state === "true" &&
+                <div className='pl-3'>
+                    <Link href={`/stream/${userData?.display_name}`}>
+                        <p className='bg-red-600 text-center rounded-sm font-bold font-roboto p-1 hover:opacity-50 w-28 xs:text-sm lg:text-lg'>
+                            GO TO LIVE
+                        </p>
+                    </Link>
                 </div>
-                {video && video.length > 0 && 
-                    <div className='md:p-5'>              
-                    <h1 className='py-3 text-lg font-semibold'>Recent streams</h1>
-                        <div className='grid 3xl:grid-cols-5 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-1'>
-                            {state === "true" ? (
-                                video.map((item) => (
-                                    <VideoCard key={item.id} item={item} />  
-                                ))
-                            ):(
-                                video.slice(0, 5).map((item) => (
-                                    <VideoCard key={item.id} item={item} />  
-                                )) 
-                            )
-                            }
-                        </div>
-                    </div>         
-                }
+            }
+            <div className='flex flex-row items-center m-5'>
+                <Image 
+                    className="rounded-full cursor-pointer" 
+                    src={userData?.profile_image_url}
+                    width={70}
+                    height={70}
+                    priority
+                    alt="user image" 
+                /> 
+                <h3 className='pl-5'>{userData?.display_name}</h3>
             </div>
+            {video && video.length > 0 && 
+                <div className='md:p-5'>              
+                <h1 className='py-3 text-lg font-semibold'>Recent streams</h1>
+                    <div className='grid 3xl:grid-cols-5 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-1'>
+                        {video.map((item) => (
+                            <VideoCard key={item.id} item={item} />  
+                        ))}
+                    </div>
+                </div>         
+            }
         </>
     );
 };
